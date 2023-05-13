d0f12111f5d8e89506d24b71df06c90dd3955a98
Lau-r-a <103428878+Lau-r-a@users.noreply.github.com>
2023-05-13 13:26:36 +0200

    add plugin to draw annotations in graphjs

--- src/app.js
+++ src/app.js
@@ -1,32 +1,64 @@
 import { Chart, registerables } from "chart.js";
+import annotationPlugin from 'chartjs-plugin-annotation';
+import hormoneData from "../doc/example_data.json";
+import hormoneReferences from "../doc/example_references.json";
+
+
 Chart.register(...registerables);
+Chart.register(annotationPlugin);
 
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-];
 
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
-  }]
-};
+function generateOptions (min, max) {
+  const options = {
+    plugins: {
+      annotation: {
+        annotations: {
+          min: {
+            type: 'line',
+            yMin: min,
+            yMax: min,
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 2,
+          },
+          max: {
+            type: 'line',
+            yMin: max,
+            yMax: max,
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 2,
+          },
+        }
+      }
+    }
+  };
+
+  return options;
+}
+
+
+
 
 const config = {
   type: 'line',
-  data,
-  options: {}
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  },
+  options: generateOptions(50, 80)
 };
 
 new Chart(
   document.getElementById('myChart'),
   config
 );
+
+//dataset min
+//dataset max
+//dataset data
+
